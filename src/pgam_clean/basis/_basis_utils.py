d9e43896613b45aadbe3db2e92825944c6db5c0b
Bence Bagi <bagi.bence94@gmail.com>
2026-03-11 11:12:57 +0100

    Add to_gam_basis

--- src/pgam_clean/basis/_basis_utils.py
+++ src/pgam_clean/basis/_basis_utils.py
@@ -1,6 +1,48 @@
 from nemos.identifiability_constraints import apply_identifiability_constraints
+from nemos.basis import AdditiveBasis, BSplineEval, MultiplicativeBasis
+from ._basis import GAMAdditiveBasis, GAMMultiplicativeBasis
+from ._bspline import GAMBSplineEval
 
 
+def to_gam_basis(basis, identifiability):
+    _type_dict = {
+        BSplineEval: GAMBSplineEval,
+        AdditiveBasis: GAMAdditiveBasis,
+        MultiplicativeBasis: GAMMultiplicativeBasis,
+    }
+
+    # don't convert if already converted
+    if isinstance(basis, tuple(_type_dict.values())):
+        return basis
+
+    if isinstance(basis, (AdditiveBasis, MultiplicativeBasis)):
+        gam_composite_type = _type_dict[type(basis)]
+
+        basis1 = to_gam_basis(basis.basis1, identifiability)
+        basis2 = to_gam_basis(basis.basis2, identifiability)
+
+        return gam_composite_type(basis1, basis2)
+
+    if isinstance(basis, BSplineEval):
+        # replace the default label
+        label = basis.label
+        if label.startswith("BSplineEval"):
+            label = label.replace("BSplineEval", "GAMBSplineEval")
+
+        return GAMBSplineEval(
+            n_basis_funcs=basis.n_basis_funcs,
+            order=basis.order,
+            bounds=basis.bounds,
+            label=label,
+            identifiability=identifiability,
+        )
+
+    raise TypeError(
+        "basis has to be one of BSplineEval, AdditiveBasis, MultiplicativeBasis"
+    )
+
+
+# TODO: Use this somewhere?
 def apply_constraints(X, add_intercept: bool = True):
     """
     Drop linearly depednent columns.
