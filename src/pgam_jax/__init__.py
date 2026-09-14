1ab23c8a4e301e7ae65af9f85e07bad198b12133
Bence Bagi <bagi.bence94@gmail.com>
2026-09-14 13:44:05 +0200

    Enable x64 by default on import except if it's explicitly disabled

--- src/pgam_jax/__init__.py
+++ src/pgam_jax/__init__.py
@@ -1,3 +1,10 @@
+import jax
+from jax._src.config import bool_env
+
+# enable x64 by default
+if bool_env("JAX_ENABLE_X64", True):
+    jax.config.update("jax_enable_x64", True)
+
 # monkey-patch nemos.basis.BSplineEval and nemos.basis.BSPlineConv to have a derivative() method
 from ._patch_nemos import *
 from .gam import GAM
